5e14b488e4a0264b6e626b792ec7749dc492395a
Bin Chen <kuotai@taobao.com>
2012-09-29 02:01:41 +0800

    fix TS-1503 if in_the_prot_queue == 0, won't be enqueue_localed to localQueue

--- iocore/eventsystem/UnixEvent.cc
+++ iocore/eventsystem/UnixEvent.cc
@@ -37,14 +37,14 @@ Event::schedule_imm(int acallback_event)
 {
   callback_event = acallback_event;
   ink_debug_assert(ethread == this_ethread());
-  if (in_the_priority_queue) {
+  if (in_the_priority_queue)
     ethread->EventQueue.remove(this);
-    ethread->EventQueueExternal.enqueue_local(this);
-  }
   timeout_at = 0;
   period = 0;
   immediate = true;
   mutex = continuation->mutex;
+  if (!in_the_prot_queue)
+    ethread->EventQueueExternal.enqueue_local(this);
 }
 
 void
@@ -53,14 +53,14 @@ Event::schedule_at(ink_hrtime atimeout_at, int acallback_event)
   callback_event = acallback_event;
   ink_debug_assert(ethread == this_ethread());
   ink_assert(atimeout_at > 0);
-  if (in_the_priority_queue) {
+  if (in_the_priority_queue)
     ethread->EventQueue.remove(this);
-    ethread->EventQueueExternal.enqueue_local(this);
-  }
   timeout_at = atimeout_at;
   period = 0;
   immediate = false;
   mutex = continuation->mutex;
+  if (!in_the_prot_queue)
+    ethread->EventQueueExternal.enqueue_local(this);
 }
 
 void
@@ -68,14 +68,14 @@ Event::schedule_in(ink_hrtime atimeout_in, int acallback_event)
 {
   callback_event = acallback_event;
   ink_debug_assert(ethread == this_ethread());
-  if (in_the_priority_queue) {
+  if (in_the_priority_queue)
     ethread->EventQueue.remove(this);
-    ethread->EventQueueExternal.enqueue_local(this);
-  }
   timeout_at = ink_get_based_hrtime() + atimeout_in;
   period = 0;
   immediate = false;
   mutex = continuation->mutex;
+  if (!in_the_prot_queue)
+    ethread->EventQueueExternal.enqueue_local(this);
 }
 
 void
@@ -84,10 +84,8 @@ Event::schedule_every(ink_hrtime aperiod, int acallback_event)
   callback_event = acallback_event;
   ink_debug_assert(ethread == this_ethread());
   ink_assert(aperiod != 0);
-  if (in_the_priority_queue) {
+  if (in_the_priority_queue)
     ethread->EventQueue.remove(this);
-    ethread->EventQueueExternal.enqueue_local(this);
-  }
   if (aperiod < 0) {
     timeout_at = aperiod;
   } else {
@@ -96,4 +94,6 @@ Event::schedule_every(ink_hrtime aperiod, int acallback_event)
   period = aperiod;
   immediate = false;
   mutex = continuation->mutex;
+  if (!in_the_prot_queue)
+    ethread->EventQueueExternal.enqueue_local(this);
 }
