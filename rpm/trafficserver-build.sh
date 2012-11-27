b92d978eee6f5f39ceaf670a20e28dec03a3a8a4
Zhao Yongming <ming.zym@gmail.com>
2012-11-27 17:43:36 +0800

    add rpm spec file for abs autobuilding
      fix building with custom release number

--- rpm/trafficserver-build.sh
+++ rpm/trafficserver-build.sh
@@ -6,3 +6,15 @@ autoreconf -i
 make asf-dist
 cd rpm
 rpm_create $2.spec -v $3 -r $4 -p /usr
+
+for mypk in `ls *.rpm`
+do
+	t_pk_l=${mypk//-/ }
+	t_pk_array=($t_pk_l)
+	ac=${#t_pk_array[*]}
+	buildnumber=${t_pk_array[$ac-1]}
+        sf=`echo $buildnumber|awk -F'.' '{print "."$(NF-2)"."$(NF-1)"."$NF}'`
+        buildnumber=${buildnumber/$sf/}
+        echo $buildnumber >  $1/rpm/BUILDNO.txt
+	break
+done
