c11ea11293aa703d682cf329a6616bd02c5a4bbe
Yunkai Zhang <qiushu.zyk@taobao.com>
2013-07-18 15:35:31 +0800

    TS-2044: Avoid Loging client running into DENY status infinitely
    When Loging host is down, Loging client will call flush_to_orphan()
    to clear send_list.
    
    And then Loging client will retry to connect to host. Once the
    connection established again, client_send() will be called and Logging
    client will move to LOG_COLL_CLIENT_IDLE status.
    
    If m_flow is equal to LOG_COLL_FLOW_DENY, there is no chance to update
    m_flow to LOG_COLL_FLOW_ALLOW.
    
    We should update m_flow to LOG_COLL_FLOW_ALLOW after clear send_list
    in flush_to_orphan().
    
    Signed-off-by: Yunkai Zhang <qiushu.zyk@taobao.com>

--- proxy/logging/LogCollationClientSM.cc
+++ proxy/logging/LogCollationClientSM.cc
@@ -744,5 +744,8 @@ LogCollationClientSM::flush_to_orphan()
     m_log_host->orphan_write_and_delete(log_buffer);
   }
 
+  // Now send_list is empty, let's update m_flow to ALOW status
+  Debug("log-coll", "[%d]client::client_send - m_flow = ALLOW", m_id);
+  m_flow = LOG_COLL_FLOW_ALLOW;
 }
 #endif // TS_MICRO
